43a8124a52a619d1a9b76847fad9a7cf2be53596
Ricwolf19 <rhtc19@gmail.com>
2025-11-08 23:19:02 -0600

    feat(Emerald IA): fps engine with Emerald IA for enemies completed, now pending to add variaty and health and damage official stats

---
+++ Assets/Emerald AI/Integrations/FPS Engine/Scripts/FPSEngineLBDBridge.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EmeraldAI
+{
+    /// <summary>
+    /// This script bridges damage between FPS Engine and Emerald AI's Location Based Damage colliders.
+    /// This allows FPS Engine to work with Location Based Damage. Note: This component is automatically added to Location Based Damage colliders (if an AI has a LBD component) so it does not need to be added manually.
+    /// </summary>
+    public class FPSEngineLBDBridge : MonoBehaviour, cowsins.IDamageable
+    {
+        LocationBasedDamageArea m_LocationBasedDamageArea;
+
+        void Start ()
+        {
+            m_LocationBasedDamageArea = GetComponent<LocationBasedDamageArea>();
+        }
+
+        public void Damage(float damage, bool IsHeadShot)
+        {
+            m_LocationBasedDamageArea.DamageArea((int)damage, null, 40, false);
+        }
+    }
+}
